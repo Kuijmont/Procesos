9fa627b37eadeb0a055c3328b1cad9a22dd71e36
Unknown <fkmontero9@gmail.com>
2018-10-24 14:09:53 +0200

    Ej 2-3-4

---
+++ Procesos/Procesos/src/ejercicios/Aleatorios.java
@@ -0,0 +1,11 @@
+package ejercicios;
+
+public class Aleatorios {
+
+	public static void main(String[]args){
+        int aleatorio = (int) (Math.random()*11);
+        System.out.println(aleatorio);        
+    }
+    
+	
+}
