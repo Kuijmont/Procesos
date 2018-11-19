4f9881b40b6e6426e846e95692abe755b096d381
Unknown <fkmontero9@gmail.com>
2018-11-19 14:07:17 +0100

    fdn

--- Procesos/Procesos_UT2/src/ej7/Areas.java
+++ Procesos/Procesos_UT2/src/ej7/Areas.java
@@ -1,11 +1,14 @@
 package ej7;
 
+import java.util.Random;
+
 public class Areas {
 
 	public static void main(String[] args) {
-		
+		Random r = new Random();
+		r.nextInt();
 		for (int i = 1; i < 11; i++) {
-			new HiloDatos(i,5).start();
+			new HiloDatos(i,r.nextInt(10)).start();
 		}	
 	}
 
