c08198b46d5cf9276b236c7d6d924213eed43744
Unknown <fkmontero9@gmail.com>
2018-11-19 14:10:26 +0100

    UT2 - complete

--- Procesos/Procesos_UT2/src/ej7/Areas.java
+++ Procesos/Procesos_UT2/src/ej7/Areas.java
@@ -6,7 +6,6 @@ public class Areas {
 
 	public static void main(String[] args) {
 		Random r = new Random();
-		r.nextInt();
 		for (int i = 1; i < 11; i++) {
 			new HiloDatos(i,r.nextInt(10)).start();
 		}	
